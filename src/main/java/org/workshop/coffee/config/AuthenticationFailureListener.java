bc6c54126eaabb92fd922eabc7af5efc5a77b8ad
Santosh <santosh.bhutkar@sonarsource.com>
2025-11-16 16:32:34 +0100

    iss

--- src/main/java/org/workshop/coffee/config/AuthenticationFailureListener.java
+++ src/main/java/org/workshop/coffee/config/AuthenticationFailureListener.java
@@ -2,20 +2,14 @@ package org.workshop.coffee.config;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class AuthenticationFailureListener implements ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
 
     private static final Logger logger = LogManager.getLogger(AuthenticationFailureListener.class);
-
-    @Autowired
-    private HttpServletRequest request;
 
     @Override
     public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
